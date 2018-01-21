ccdab9f9444ac874988edb4fd0dd918c7000ddb7
Kaushik Mahorker <kmahorker@gmail.com>
2018-01-21 05:58:17 -0800

    asdf adsljhfasdlkjf;kadsl

--- app/javascript/containers/create.jsx
+++ app/javascript/containers/create.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Button from 'react-bootstrap/lib/Button';
-import FormControl from 'react-bootstrap/lib/FormControl';
+import TextInput  from 'react-bootstrap/lib/TextInput';
+import FormControl  from 'react-bootstrap/lib/FormControl';
+import FormGroup  from 'react-bootstrap/lib/FormGroup';
+
 
 
 const idea_url = "/posts";
@@ -24,17 +27,22 @@ export default class Create extends React.Component {
     render() {
         return (
             <div>
-                <FormControl type={"text"} placeholder={"Title"}inputRef={ref => { this.input = ref; }} ref =>{"title"}/><br/>
-                <FormControl placeholder={"Description"} ref={"description"} aria-rowcount="5" aria-colcount="40"/><br/>
-                <FormControl placeholder={"Hours"} type={"number"} ref={"hours"}/><br/>
-                <FormControl placeholder={"Weeks"} type={"number"} ref={"weeks"}/><br/>
-                <FormControl placeholder={"Email"} type={"email"} ref={"email"}/><br/>
-                <FormControl placeholder={"Github"} ref={"github"}/><br/>
+                    <FormGroup
+                        controlId="formBasicText"
+                        validationState={this.getValidationState()}
+                    >
+                        <FormControl type={"text"} placeholder={"Title"} ref =>{"title"}> </FormControl> <br/>
+                        <FormControl placeholder={"Description"} ref={"description"} aria-rowcount="5" aria-colcount="40"/><br/>
+                        <FormControl placeholder={"Hours"} type={"number"} ref={"hours"}/><br/>
+                        <FormControl placeholder={"Weeks"} type={"number"} ref={"weeks"}/><br/>
+                        <FormControl placeholder={"Email"} type={"email"} ref={"email"}/><br/>
+                        <FormControl type= {"text"} placeholder={"Github"} ref={"github"}/><br/>
+                    </FormGroup>
 
                 {/*<form ref={"skills"}>*/}
                     {/*<input type="checkbox" value={2}>Java</input>*/}
                 {/*</form>*/}
-                {/*Put skills here*/}<br/>
+                {/*Put skills here*/}
 
                 <Button bsStyle="primary"
                         onClick={this.submit.bind(this)}>Create</Button>
