ef237edf18147b47f186004ec5670b2cdcb879c5
Kaushik Mahorker <kmahorker@gmail.com>
2018-01-20 20:55:30 -0800

    Set upvote_count default to 0

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+  before_save :default_values
+  def default_values
+    self.upvote_count ||= 0 # note self.status = 'P' if self.status.nil? might be safer (per @frontendbeauty)
+  end
 end
