98ea02ea13d7cb21fcd6975c237ed06bd34aab0a
vikr01 <vik120@icloud.com>
2018-01-21 01:49:58 -0800

    input boxes

---
+++ app/javascript/containers/create.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const idea_url = "/create";
+
+export default class Create extends React.Component {
+    submit() {
+        let data = {};
+        _.each(this.refs, function (ref, index) {
+            data[index] = ref.value;
+        });
+        delete data.skills;
+
+
+
+        $.post(idea_url, data)
+            .done(function (result) {
+                this.props.onResult(result);
+            }.bind(this));
+    }
+
+    render() {
+        return (
+            <div>
+                <input placeholder={"Title"} ref={"title"}/><br/>
+                <input placeholder={"Description"} ref={"description"} aria-rowcount="5" aria-colcount="40"/><br/>
+                <input placeholder={"Hours"} type={"number"} ref={"hours"}/><br/>
+                <input placeholder={"Weeks"} type={"number"} ref={"weeks"}/><br/>
+                <input placeholder={"Email"} type={"email"} ref={"email"}/><br/>
+                <input placeholder={"Github"} ref={"github"}/><br/>
+
+                {/*<form ref={"skills"}>*/}
+                    {/*<input type="checkbox" value={2}>Java</input>*/}
+                {/*</form>*/}
+                {/*Put skills here*/}<br/>
+
+                <button onClick={this.submit.bind(this)}/>
+            </div>
+        );
+    }
+}
