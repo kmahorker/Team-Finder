5f8a547ea8c66e172c015f0aa7437c13feb50d60
Kaushik Mahorker <kmahorker@gmail.com>
2018-01-21 06:44:48 -0800

    fix bug

--- app/javascript/containers/create.jsx
+++ app/javascript/containers/create.jsx
@@ -1,10 +1,4 @@
 import React from 'react';
-import Button from 'react-bootstrap/lib/Button';
-// import TextInput  from 'react-bootstrap/lib/TextInput';
-import FormControl  from 'react-bootstrap/lib/FormControl';
-import FormGroup  from 'react-bootstrap/lib/FormGroup';
-
-
 
 const idea_url = "/posts";
 
@@ -27,24 +21,19 @@ export default class Create extends React.Component {
     render() {
         return (
             <div>
-                    {/*<FormGroup*/}
-                {/*controlId="formBasicText"*/}
-                {/*validationState={this.getValidationState()}*/}
-                {/*>*/}
-                <input type={"text"} placeholder={"Title"} ref ={"title"}> </input> <br/>
+                <input placeholder={"Title"} ref={"title"}/><br/>
                 <input placeholder={"Description"} ref={"description"} aria-rowcount="5" aria-colcount="40"/><br/>
                 <input placeholder={"Hours"} type={"number"} ref={"hours"}/><br/>
                 <input placeholder={"Weeks"} type={"number"} ref={"weeks"}/><br/>
                 <input placeholder={"Email"} type={"email"} ref={"email"}/><br/>
-                <input type= {"text"} placeholder={"Github"} ref={"github"}/><br/>
+                <input placeholder={"Github"} ref={"github"}/><br/>
 
                 {/*<form ref={"skills"}>*/}
                     {/*<input type="checkbox" value={2}>Java</input>*/}
                 {/*</form>*/}
-                {/*Put skills here*/}
+                {/*Put skills here*/}<br/>
 
-                <Button bsStyle="primary"
-                        onClick={this.submit.bind(this)}>Create</Button>
+                <button onClick={this.submit.bind(this)}>Create</button>
             </div>
         );
     }
